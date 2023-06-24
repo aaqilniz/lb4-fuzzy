c42a4c4adf8f8ffa8784048d213365bdf7e3d081
Muhammad Aaqil <aaqilniz@yahoo.com>
2023-06-24 17:45:05 +0500

    initial
    Signed-off-by: Muhammad Aaqil <aaqilniz@yahoo.com>

---
+++ cli.js
@@ -0,0 +1,316 @@
+const chalk = require('chalk');
+const yargs = require('yargs/yargs');
+const fs = require('fs');
+const path = require('path');
+const pluralize = require('pluralize');
+
+const {
+  isLoopBackApp,
+  updateFile,
+  addImports,
+  execute,
+  replaceText,
+  toPascalCase,
+  toCamelCase,
+  log,
+} = require('./utils');
+
+module.exports = async () => {
+  let {
+    fuzzy,
+    centralFuzzy,
+    datasource,
+    config,
+  } = yargs(process.argv.slice(2)).argv;
+  
+  const invokedFrom = process.cwd();
+  const applicationPath = `${invokedFrom}/src/application.ts`;
+
+  if(config && typeof config === 'string') {
+    config = JSON.parse(config);
+    centralFuzzy = config.centralFuzzy;
+    fuzzy = config.fuzzy;
+    datasource = config.datasource;
+  }
+
+  if((!fuzzy && !centralFuzzy) || !datasource) {
+    throw Error('please pass configs: fuzzy or centralFuzzy and datasource');
+  }
+
+  const package = require(`${invokedFrom}/package.json`);
+
+  log(chalk.blue('Confirming if this is a LoopBack 4 project.'));
+  if (!isLoopBackApp(package)) throw Error('Not a loopback project');
+  
+
+  /*******Creating Centeral fuzzy search*******/
+  if(centralFuzzy) {
+    const controllerPath = `${invokedFrom}/src/controllers/fuzzy-search.controller.ts`;
+    if (!fs.existsSync(controllerPath)) {
+      log(chalk.blue('Creating fuzzy search controller.'));
+      fs.copyFileSync(path.join(__dirname, './text-codes/fuzzy-search.controller.ts.txt'), controllerPath);
+    }
+    // replacing the datasource provided
+    replaceText(controllerPath, 'DbDataSource', datasource,);
+    
+    // exporting central fuzzy-search endpoint from controllers/index.ts
+    const controllerIndexPath = `${invokedFrom}/src/controllers/index.ts`;
+    log(chalk.blue('Updating controllers/index.ts'));
+    updateFile(
+      controllerIndexPath,
+      'export * from \'./ping.controller\';',
+      'export * from \'./fuzzy-search.controller\';'
+      );
+    
+    const deps = package.dependencies;
+    const pkg = 'fuse.js';
+    if (!deps[pkg]) {
+      execute(`npm i ${pkg}`, `Installing ${pkg}`, 'installing fues.js');
+    }
+    await generateServices(invokedFrom);
+    log(chalk.bold(chalk.green('Successfully generated central fuzzy search API.')));
+  }
+
+  /*******Creating fuzzy search for each route*******/
+  if(fuzzy) {
+    log(chalk.blue('***Generating fuzzy endpoint for each controller***'));
+    // reading models
+    const modelDirPath = `${invokedFrom}/src/models`;
+    const fileNames = fs.readdirSync(modelDirPath);
+    const modelNames = [];
+    fileNames.forEach(fileName => {
+      if(fileName !== 'README.md' && fileName !== 'index.ts') {
+        const modelName = fileName.split('.model.ts')[0];
+        modelNames.push(modelName);
+      }
+    });
+    if(!modelNames.length) {
+      throw Error('No model found. Please run the cli after generating models.');
+    }
+    modelNames.forEach(modelName => {
+      if (!fs.existsSync(`${invokedFrom}/src/repositories/${modelName}.repository.ts`)) {
+        throw Error(`Repository for ${modelName} model is not found. Please run the cli after generating repositories.`);
+      }
+    });
+    modelNames.forEach(model => {
+      const eachControllerPath = `${invokedFrom}/src/controllers/${model}.controller.ts`;
+      let camelCasedModel = model;
+      if(camelCasedModel.includes('-')) {
+        const replacedModel = camelCasedModel.replaceAll('-', ' ');
+        camelCasedModel = toCamelCase(replacedModel);
+      }
+      const pluralizedModel = pluralize(model);
+      if (fs.existsSync(eachControllerPath)) {
+        const fuzzySearchAPI = `
+          @get('/${pluralizedModel}/fuzzy/{searchTerm}', {
+            responses: {
+                '200': {
+                  description: 'Array of ${pluralizedModel} model instances',
+                  content: {
+                    'application/json': {
+                      schema: {
+                        type: 'array',
+                        items: getModelSchemaRef(${toPascalCase(camelCasedModel)}, {includeRelations: true}),
+                      },
+                    },
+                  },
+                },
+              },
+            })
+            async fuzzySearch(): Promise<${toPascalCase(pluralizedModel)}[]> {
+              return this.${pluralizedModel}Repository.find();
+            }
+          `;
+        // updating existing controller file to add fuzzy search endpoint
+        log(chalk.blue(`Adding fuzzy endpoint to ${pluralizedModel}`));
+        updateFile(
+          eachControllerPath,
+          `) {}`,
+          fuzzySearchAPI,
+          );
+      } else {
+        log(chalk.blue(`Generating controller file for ${model}`));
+        fs.writeFileSync(eachControllerPath, `import {repository} from '@loopback/repository';`);
+        const fuzzySearchAPIWithClass = `
+          export class ${toPascalCase(model)}Controller {
+            constructor(
+              @repository(${toPascalCase(camelCasedModel)}Repository)
+              public ${camelCasedModel}Repository: ${toPascalCase(camelCasedModel)}Repository,
+            ) {}
+            @get('/${pluralizedModel}/fuzzy/{searchTerm}', {
+              responses: {
+                  '200': {
+                    description: 'Array of ${pluralizedModel} model instances',
+                    content: {
+                      'application/json': {
+                        schema: {
+                          type: 'array',
+                          items: getModelSchemaRef(${toPascalCase(camelCasedModel)}, {includeRelations: true}),
+                        },
+                      },
+                    },
+                  },
+                },
+              })
+              async fuzzySearch(): Promise<${toPascalCase(camelCasedModel)}[]> {
+                return this.${camelCasedModel}Repository.find();
+              }
+            }
+          `;
+        updateFile(
+          eachControllerPath,
+          `import {repository} from '@loopback/repository';`,
+          fuzzySearchAPIWithClass,
+        );
+        addImports(eachControllerPath, [
+          `import {${toPascalCase(camelCasedModel)}Repository} from '../repositories';`,
+          `import {${toPascalCase(camelCasedModel)}} from '../models';`,
+          `import { get, getModelSchemaRef, } from '@loopback/rest';`,
+          `import {${toPascalCase(camelCasedModel)}Repository} from '../repositories';`,
+          `import {${toPascalCase(camelCasedModel)}} from '../models';`
+        ]);
+
+        const controllerIndexPath = `${invokedFrom}/src/controllers/index.ts`;
+        log(chalk.blue(`Updating controller/index.ts file to add ${model}`));
+        updateFile(
+          controllerIndexPath,
+          `export * from './ping.controller';`,
+          `export * from './${model}.controller';`
+        );
+        
+      }
+    });
+    await generateServices(invokedFrom);
+    /*******Creating fuzzy search interceptor under interceptors*******/
+    const interceptorPath = `${invokedFrom}/src/interceptors/fuzzy.interceptor.ts`;
+    if (!fs.existsSync(interceptorPath)) {
+      log(chalk.blue('Generating fuzzy interceptor.'));
+      execute(`lb4 interceptor --config '{"name": "fuzzy", "global":true, "group": "", "yes": true}'`, 'Generating interceptor');
+      
+    }
+    replaceText(
+      interceptorPath,
+      `Interceptor`,
+      `Interceptor, inject`
+    )
+    // adding code to intercept
+    updateFile(
+      interceptorPath,
+      `return result;`,
+      `const request = this.requestContext.request;
+      const segments = request.path.split('/');
+      // Check if the route contains 'fuzzy' and the result is non-empty array
+      if (
+        segments.includes('fuzzy') &&
+        Array.isArray(result) &&
+        result.length > 0 &&
+        typeof result[0] === 'object'
+      ) {
+        const modelProperties = this.getModelProperties(result[0]);
+        const options: FuzzySearchOptions = {
+          includeScore: true,
+          includeMatches: true,
+          minMatchCharLength: 3,
+          threshold: 0.4,
+          ignoreLocation: true,
+          keys: modelProperties,
+        };
+        const searchTerm = segments[segments.indexOf('fuzzy') + 1];
+        if (searchTerm) {
+          let searchResult = this.FuzzySearchService.search(
+            result,
+            searchTerm,
+            options,
+          );
+          searchResult = searchResult.map((item: any) => {
+            return {
+              ...item,
+              modelName: result[0].constructor.name, // Name of the model
+            };
+          });
+          return searchResult;
+        }
+      }
+      `,
+      true
+      );
+    // adding BINDING_KEY
+    updateFile(
+      interceptorPath,
+      `) { }`,
+      `static readonly BINDING_KEY = BindingKey.create<FuzzyInterceptor>(
+        'interceptors.FuzzyInterceptor',
+      );`
+    );
+    
+    // add getModelProperties method
+    updateFile(
+      interceptorPath,
+      `async intercept(`,
+      `getModelProperties(modelInstance: Model): string[] {
+        const modelClass = modelInstance.constructor as typeof Entity;
+        const modelDefinition = modelClass.definition;
+        if (!modelDefinition) {
+          return [];
+        }
+        return Object.keys(modelDefinition.properties);
+      }`,
+      true
+      );
+    // add constructor
+    updateFile(
+      interceptorPath,
+      `value() {`,
+      `constructor(
+        @inject('services.FuzzySearchService')
+        private FuzzySearchService: FuzzySearchService,
+        @inject(RestBindings.Http.CONTEXT)
+        private requestContext: RequestContext,
+      ) { }`,
+      true
+      );
+    //adding required imports service and BindKey from context
+    addImports(interceptorPath, [
+      `import {BindingKey} from '@loopback/context';`,
+      `import {FuzzySearchOptions, FuzzySearchService} from '../services';`,
+      `import {Entity} from '@loopback/repository';`,
+      `import {Model, RequestContext, RestBindings} from '@loopback/rest';`
+    ]);
+    log(chalk.bold(chalk.green('Successfully generated fuzzy search APIs for every controller.')));
+  }
+}
+
+const generateServices = async (invokedFrom) => {
+  log(chalk.blue('****Generating fuzzy search service.****'));
+  const servicesPath = `${invokedFrom}/src/services/fuzzy-search.service.ts`;
+  if (!fs.existsSync(servicesPath)) {
+    execute(`lb4 service --config '{"type": "class", "name": "fuzzySearch", "yes": true}'`, 'generating fuzzy service');
+  }
+  addImports(servicesPath, [`import Fuse from 'fuse.js';`]);
+  updateFile(
+    servicesPath,
+    `@injectable({scope: BindingScope.TRANSIENT})`,
+    `export interface FuzzySearchOptions {
+      includeScore?: boolean;
+      includeMatches?: boolean;
+      minMatchCharLength?: number;
+      threshold?: number;
+      ignoreLocation?: boolean;
+      keys: string[];
+    }`,
+    true
+  );
+  updateFile(
+    servicesPath,
+    `constructor(/* Add @inject to inject parameters */) {}`,
+    `search<T>(
+      data: T[],
+      searchTerm: string,
+      options: FuzzySearchOptions,
+    ): Fuse.FuseResult<T>[] {
+      const fuseIndex = Fuse.createIndex(options.keys, data);
+      const fuse = new Fuse(data, options, fuseIndex);
+      return fuse.search(searchTerm);
+    }`
+  );
+}
---
+++ utils.js
@@ -0,0 +1,62 @@
+const chalk = require('chalk');
+const { execSync } = require('child_process');
+const fs = require('fs');
+
+module.exports.log = console.log;
+
+module.exports.kebabCase = string => string
+  .replace(/([a-z])([A-Z])/g, "$1-$2")
+  .replace(/[\s_]+/g, '-')
+  .toLowerCase();
+
+module.exports.toPascalCase = string => string
+  .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
+  .map(x => x.charAt(0).toUpperCase() + x.slice(1).toLowerCase())
+  .join('');
+
+module.exports.toCamelCase = (string) => string
+  .replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function(match, index) {
+    if (+match === 0) return ''; 
+    return index === 0 ? match.toLowerCase() : match.toUpperCase();
+  });
+
+module.exports.isLoopBackApp = (package) => {
+  if (!package) return false;
+  const { dependencies } = package;
+  if (!dependencies['@loopback/core']) return false;
+  return true;
+}
+
+module.exports.replaceText = (filePath, updateThis, updateWith, replaceAll) => {
+  const file = fs.readFileSync(filePath, 'utf8');
+  if (file.indexOf(updateWith) === -1) {
+    const updatedFile = file[replaceAll ? 'replaceAll' : 'replace'](
+      updateThis,
+      updateWith
+    );
+    fs.writeFileSync(filePath, updatedFile, 'utf8');
+  }
+}
+
+module.exports.updateFile = (filePath, updateThis, updateWith, pre, replaceAll) => {
+  const file = fs.readFileSync(filePath, 'utf8');
+  if (file.indexOf(updateWith) === -1) {
+    const updateWithText = pre ? updateWith + '\n' + updateThis : updateThis + '\n\t' + updateWith;
+    this.replaceText(filePath, updateThis, updateWithText, replaceAll);
+  }
+}
+
+module.exports.addImports = (filePath, newImports) => {
+  newImports.forEach(newImport => {
+    this.updateFile(filePath, 'import', newImport, true);
+  });
+}
+
+module.exports.execute = (command, message) => {
+  this.log(chalk.blue(message));
+  try {
+    execSync(command, {stdio: 'inherit'});
+  } catch (err) {
+    throw Error(`failed to execute ${command}`);
+  }
+}
