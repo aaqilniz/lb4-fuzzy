c0108dc02f716828130c375f589c16736d9533c7
Muhammad Aaqil <aaqilcs102@gmail.com>
2024-05-04 00:20:30 +0500

    fix: different approach for one/multiwords
    Signed-off-by: Muhammad Aaqil <aaqilcs102@gmail.com>

--- cli.js
+++ cli.js
@@ -251,7 +251,7 @@ module.exports = async () => {
 
         let searchTerm = segments[segments.indexOf('fuzzy') + 1];
         searchTerm = searchTerm.replace(/%20/g, ' ');
-        if(searchTerm.split(' ').length) {
+        if(searchTerm.split(' ').length > 1) {
           searchTerm = searchTerm.split(' ').map(word => \`'\${word}\`).join(' ');
           searchTerm = searchTerm.replace(/ /g, ' | ');
         }
